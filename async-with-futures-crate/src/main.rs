12f7751bd0b3cd66b24d11c633cc72d5c741ecc3
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-27 00:52:54 +0530

    Naive way of working with Multiple async tasks
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sun Aug 27 00:52:54 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	modified:   async-with-futures-crate/src/main.rs

--- async-with-futures-crate/src/main.rs
+++ async-with-futures-crate/src/main.rs
@@ -13,6 +13,13 @@ fn main() {
     // given future complete it's excution
     let val = block_on(fut);
 
+    // naive approach could be as following
+    // but this would be still synchronous
+    // We will be doing synchronously hello -> watching -> snacking
+    // Is there any other way to invoke future?
+    block_on(watch_favourite_show());
+    block_on(have_favourite_snacks());
+
     println!("Value returned from the future is {:01}", val);
 }
 
@@ -20,4 +27,14 @@ async fn hello_async_world() -> i32 {
     println!("Hello ASYNC world");
     // return my fancy integer
     32
+}
+
+// Watch your favourite show
+async fn watch_favourite_show() {
+    println!("I'm watching RAMAYANA");
+}
+
+// Have your favourite snacks
+async fn have_favourite_snacks() {
+    println!("I'm eating BANANA CHIPS");
 }
