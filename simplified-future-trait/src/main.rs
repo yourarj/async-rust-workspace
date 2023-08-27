84c29b5ab91c698a456019cd5a84a1b9a95af7ed
yourarj <11359226+yourarj@users.noreply.github.com>
2023-08-27 12:04:51 +0530

    simplified future trait
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sun Aug 27 12:04:51 2023 +0530
    
     On branch main
    
     Changes to be committed:
    	modified:   Cargo.lock
    	modified:   Cargo.toml
    	modified:   async-with-futures-crate/Cargo.toml
    	new file:   simplified-future-trait/Cargo.toml
    	new file:   simplified-future-trait/src/main.rs

---
+++ simplified-future-trait/src/main.rs
@@ -0,0 +1,30 @@
+/// Simplified version of actual Future trait
+trait SimpleFutureTrait {
+    /// represents value that the future will return
+    type Output;
+
+    /// method to compute and retrieve futures output
+    /// A future can only advance by calling poll only
+    /// it's inert otherwise
+    ///
+    /// 🤔 What is wake: fn()
+    /// When we poll a future, if computation compeletes Poll::Ready(RETURN_TYPE_VAL)
+    /// But this is not the case all the time
+    /// In other cases it return POLL::Pending and make arrangements for wake to get invoked
+    /// when future can make progress
+    /// When wake is Called EXECUTOR will again call poll and make progress
+    /// The iteration repeats till future completes it's exectuion.  
+    fn poll(&mut self, wake: fn()) -> Poll<Self::Output>;
+}
+
+/// Represents state of future computation
+enum Poll<T> {
+    /// Future completed computation and result is READY
+    Ready(T),
+    /// Future is under progress
+    Pending,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
